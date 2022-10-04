8cbe40159e926690fba2c442e6123e5cb0f4fb22
Marius Synowiec <Marius.Synowiec@secunet.com>
2022-10-04 17:09:50 +0200

    fixed App.js

--- src/App.js
+++ src/App.js
@@ -3,18 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
-function App({ isPassedToWithAuthenticator, signOut, user }) {
-    if (!isPassedToWithAuthenticator) {
-        throw new Error(`isPassedToWithAuthenticator was not provided`);
-    }
-
+function App() {
     return (
         <div className="App">
             <header>
                 <img src={logo} className="App-logo" alt="logo" />
                 <h1>We now have Auth!</h1>
             </header>
-            <button onClick={signOut}>Sign out</button>
+            <AmplifySignOut />
         </div>
     );
 }
